15521bdba4c33cadd0734293482e1a3b84d8b28a
osh418 <brown8167@gmail.com>
2025-06-07 20:04:35 +0900

    [프로그래머스][LEVEL 2] 제목: 기능개발 - solution1 및 solution2 함수 추가 및 성능 측정 코드 포함

--- 프로그래머스/스택큐/기능개발/index.js
+++ 프로그래머스/스택큐/기능개발/index.js
@@ -1,4 +1,4 @@
-function solution(progresses, speeds) {
+function solution1(progresses, speeds) {
     var answer = [];
     
     var a = progresses.map((v, i) => Math.ceil((100 - v) / speeds[i]))
@@ -21,3 +21,44 @@ function solution(progresses, speeds) {
     
     return answer;
 }
+
+function solution2(progresses, speeds) {
+    const answer = [];
+    const daysQueue = progresses.map((progress, index) => 
+        Math.ceil((100 - progress) / speeds[index])
+    );
+    
+    while (daysQueue.length > 0) {
+        const current = daysQueue.shift();
+        let count = 1;
+
+        const peek = daysQueue[0];
+        
+        while (daysQueue.length > 0 && peek <= current) {
+            daysQueue.shift();
+            count++;
+        }
+        
+        answer.push(count);
+    }
+
+    return answer;
+}
+
+/**
+
+const progresses = [93, 30, 55];
+const speeds = [1, 30, 5];
+
+// solution: 5.216ms
+console.time('solution1');
+console.log(solution1(progresses, speeds));
+console.timeEnd('solution1');
+
+
+// solution:5.301ms
+console.time('solution2');
+console.log(solution2(progresses, speeds));
+console.timeEnd('solution2');
+
+*/
