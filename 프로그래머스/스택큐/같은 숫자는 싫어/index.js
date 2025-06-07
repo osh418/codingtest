687b8558c0d9979a337b024ca2bd78c6aed68cd1
osh418 <brown8167@gmail.com>
2025-06-07 19:34:18 +0900

    [프로그래머스][LEVEL 2] 제목: 같은 숫자는 싫어 - solution2 추가

--- 프로그래머스/스택큐/같은 숫자는 싫어/index.js
+++ 프로그래머스/스택큐/같은 숫자는 싫어/index.js
@@ -1,4 +1,4 @@
-function solution(arr)
+function solution1(arr)
 {
     var answer = [];
     
@@ -6,3 +6,56 @@ function solution(arr)
     
     return answer;
 }
+
+function solution2(arr)
+{
+    var stack = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        const peek = stack[stack.length - 1];
+        if (peek !== arr[i]) {
+            stack.push(arr[i]);
+        }
+    }
+    
+    return stack;
+}
+
+/**
+const arr = [1, 1, 3, 3, 0, 1, 1];
+// solution1: 5.186ms
+console.time('solution1');
+console.log(solution1(arr));
+console.timeEnd('solution1');
+
+// solution2: 0.063ms
+console.time('solution2');
+console.log(solution2(arr));
+console.timeEnd('solution2');
+*/
+
+/**
+const arr = [4,4,4,3,3];
+// solution1: 4.957ms
+console.time('solution1');
+console.log(solution1(arr));
+console.timeEnd('solution1');
+
+// solution2: 0.063ms
+console.time('solution2');
+console.log(solution2(arr));
+console.timeEnd('solution2');
+*/
+
+/**
+const arr = Array.from({ length: 100000 }, (_, i) => (i + 1) % 2);
+// solution1: 6.996ms
+console.time('solution1');
+console.log(solution1(arr));
+console.timeEnd('solution1');
+
+// solution2: 1.751ms
+console.time('solution2');
+console.log(solution2(arr));
+console.timeEnd('solution2');
+*/
