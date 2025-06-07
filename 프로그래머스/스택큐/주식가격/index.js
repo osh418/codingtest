e0e33a48b11d00a1e003a8b4b7c885d29cb78691
osh418 <brown8167@gmail.com>
2025-06-07 18:37:04 +0900

    [프로그래머스][LEVEL 2] 제목: 주식가격 - solution3 함수 제거 및 관련 코드 정리

--- 프로그래머스/스택큐/주식가격/index.js
+++ 프로그래머스/스택큐/주식가격/index.js
@@ -35,27 +35,6 @@ function solution2(prices) {
     return answers;
 }
 
-function solution3(prices) {
-    const n = prices.length;
-    const answer = new Array(n).fill(0);
-
-    for (let i = n - 2; i >= 0; i--) {
-        let j = i + 1;
-        // 다음 가격이 더 크거나 같으면, 정답을 더해서 jump
-        while (j < n && prices[i] <= prices[j]) {
-            // 미리 계산된 값을 활용해 건너뛰기
-            if (answer[j] === 0) {
-                j++; // 더 이상 점프할 곳이 없음
-            } else {
-                j += answer[j];
-            }
-        }
-        answer[i] = j - i;
-    }
-
-    return answer;
-}
-
 // console.log(solution([1, 2, 3, 2, 3]));
 const prices = Array.from({ length: 100000 }, (_, i) => i + 1).concat(Array.from({ length: 100000 }, (_, i) => i + 1)).concat(Array.from({ length: 100000 }, (_, i) => i + 1)).concat(Array.from({ length: 100000 }, (_, i) => i + 1));
 
@@ -68,8 +47,3 @@ console.timeEnd('solution1');
 console.time('solution2');
 console.log(solution2(prices));
 console.timeEnd('solution2');
-
-// solution3: 4.111ms
-console.time('solution3');
-console.log(solution3(prices));
-console.timeEnd('solution3');
